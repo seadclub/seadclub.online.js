850fce9c6c4c6613ff6142af8301ad1e32370d49
kenjitheman <kenjith3man@gmail.com>
2023-11-19 22:20:39 +0200

    chore: Update footer

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,20 @@
 import { Box } from "@chakra-ui/react";
 
 const Footer = () => {
-  return (
-    <Box align="center" opacity={0.4} fontSize="sm">
-      <a href="https://github.com/kenjitheman/seadclub.online">
-        <p>
-          source code
-        </p>
-      </a>
-      <br />
-      &copy; {new Date().getFullYear()}{" "}
-      Клуб Інженерії Програмного Забезпечення та Цифрових Технологій
-      <p>Державний університет інформаційно-комунікаційних технологій</p>
-      <p>Bсі права захищені</p>
-    </Box>
-  );
+    return (
+        <Box align="center" opacity={0.4} fontSize="sm">
+            <a href="https://github.com/seadclub/seadclub.online">
+                <p>
+                    source code
+                </p>
+            </a>
+            <br />
+            &copy; {new Date().getFullYear()}{" "}
+            Клуб Інженерії Програмного Забезпечення та Цифрових Технологій
+            <p>Державний університет інформаційно-комунікаційних технологій</p>
+            <p>Bсі права захищені</p>
+        </Box>
+    );
 };
 
 export default Footer;
