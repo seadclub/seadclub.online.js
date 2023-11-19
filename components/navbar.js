2e4431ec7374ffc49a55c8c6e32f14e26bf91fe0
kenjitheman <kenjith3man@gmail.com>
2023-11-19 22:19:50 +0200

    chore: Major navbar update

--- components/navbar.js
+++ components/navbar.js
@@ -2,144 +2,150 @@ import { forwardRef } from "react";
 import Logo from "./logo";
 import NextLink from "next/link";
 import {
-  Box,
-  Container,
-  Flex,
-  Heading,
-  IconButton,
-  Link,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Stack,
-  useColorModeValue,
+    Box,
+    Container,
+    Flex,
+    Heading,
+    IconButton,
+    Link,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Stack,
+    useColorModeValue,
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import ThemeToggleButton from "./theme-toggle-button";
 import { IoLogoGithub } from "react-icons/io5";
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
-  const active = path === href;
-  const inactiveColor = useColorModeValue("gray.800", "whiteAlpha.900");
-  return (
-    <Link
-      as={NextLink}
-      href={href}
-      scroll={false}
-      p={2}
-      bg={active ? "grassTeal" : undefined}
-      color={active ? "#202023" : inactiveColor}
-      target={target}
-      {...props}
-    >
-      {children}
-    </Link>
-  );
+    return (
+        <Link
+            as={NextLink}
+            href={href}
+            scroll={false}
+            py={2}
+            px={4}
+            bg={useColorModeValue('transparent', 'transparent')}
+            outline="1px solid"
+            outlineColor={useColorModeValue('gray.300', 'gray.700')}
+            rounded="xl"
+            color={useColorModeValue('black', 'white')}
+            border="1px solid transparent"
+            fontSize="lg"
+            target={target}
+            {...props}
+        >
+            {children}
+        </Link>
+    );
 };
 
 const MenuLink = forwardRef((props, ref) => (
-  <Link ref={ref} as={NextLink} {...props} />
+    <Link ref={ref} as={NextLink} {...props} />
 ));
 
 const Navbar = (props) => {
-  const { path } = props;
+    const { path } = props;
 
-  return (
-    <Box
-      position="fixed"
-      as="nav"
-      w="100%"
-      bg={useColorModeValue("#ffffff40", "#20202380")}
-      css={{ backdropFilter: "blur(10px)" }}
-      zIndex={2}
-      {...props}
-    >
-      <Container
-        display="flex"
-        p={2}
-        maxW="container.md"
-        wrap="wrap"
-        align="center"
-        justify="space-between"
-      >
-        <Flex align="center" mr={5}>
-          <Heading as="h1" size="lg" letterSpacing={"tighter"}>
-            <Logo />
-          </Heading>
-        </Flex>
-
-        <Stack
-          direction={{ base: "column", md: "row" }}
-          display={{ base: "none", md: "flex" }}
-          width={{ base: "full", md: "auto" }}
-          alignItems="center"
-          flexGrow={1}
-          mt={{ base: 4, md: 0 }}
+    return (
+        <Box
+            position="fixed"
+            as="nav"
+            w="100%"
+            bg={useColorModeValue("transparent", "transparent")}
+            css={{ backdropFilter: "blur(10px)" }}
+            justifyContent="space-between"
+            borderBottom="1px solid"
+            borderBottomColor={useColorModeValue("gray.200", "gray.900")}
+            zIndex={2}
+            {...props}
         >
-          <LinkItem
-            href="https://forms.gle/h1CaPN3fyBbhHrJC7"
-            path={path}
-          >
-            Bступити в клуб
-          </LinkItem>
-          <LinkItem href="https://t.me/seadclub_bot" path={path}>
-            Зв'язатися з нами
-          </LinkItem>
-          <LinkItem
-            target="_blank"
-            href="https://github.com/seadclub"
-            path={path}
-            display="inline-flex"
-            alignItems="center"
-            style={{ gap: 4 }}
-            pl={2}
-          >
-            <IoLogoGithub />
-            Github
-          </LinkItem>
-        </Stack>
+            <Container
+                display="flex"
+                p={2}
+                maxW="container.xl"
+                wrap="wrap"
+                align="center"
+                justifyContent="space-between"
+            >
+                <Flex align="center" mr={5}>
+                    <Heading as="h1" size="xl" letterSpacing={"tighter"}>
+                        <Logo />
+                    </Heading>
+                </Flex>
 
-        <Box flex={1} align="right">
-          <ThemeToggleButton />
+                <Stack
+                    direction={{ base: "column", md: "row" }}
+                    display={{ base: "none", md: "flex" }}
+                    width={{ base: "full", md: "auto" }}
+                    alignItems="center"
+                    flexGrow={1}
+                    mt={{ base: 4, md: 0 }}
+                >
+                    <LinkItem
+                        href="http://join.seadclub.online"
+                        path={path}
+                    >
+                        Bступити в клуб
+                    </LinkItem>
+                    <LinkItem href="https://t.me/seadclub_bot" path={path}>
+                        Зв'язатися з нами
+                    </LinkItem>
+                    <LinkItem
+                        target="_blank"
+                        href="https://github.com/seadclub"
+                        path={path}
+                        display="inline-flex"
+                        alignItems="center"
+                        style={{ gap: 4 }}
+                        p={2}
+                    >
+                        <IoLogoGithub size={35} />
+                    </LinkItem>
+                </Stack>
 
-          <Box ml={2} display={{ base: "inline-block", md: "none" }}>
-            <Menu isLazy id="navbar-menu">
-              <MenuButton
-                as={IconButton}
-                icon={<HamburgerIcon />}
-                variant="outline"
-                aria-label="Options"
-              />
-              <MenuList>
-                <MenuItem
-                  as={MenuLink}
-                  href="https://forms.gle/h1CaPN3fyBbhHrJC7"
-                >
-                  Bступити в клуб
-                </MenuItem>
-                <MenuItem as={MenuLink} href="https://t.me/seadclub_bot">
-                  Зв'язатися з нами
-                </MenuItem>
-                <MenuItem
-                  as={MenuLink}
-                  href="https://github.com/seadclub"
-                >
-                  Github
-                </MenuItem>
-                <MenuItem
-                  as={Link}
-                  href="https://github.com/kenjitheman/seadclub.online"
-                >
-                  Source code
-                </MenuItem>
-              </MenuList>
-            </Menu>
-          </Box>
+                <Box flex={1} align="right">
+                    <ThemeToggleButton />
+
+                    <Box ml={2} display={{ base: "inline-block", md: "none" }}>
+                        <Menu isLazy id="navbar-menu">
+                            <MenuButton
+                                as={IconButton}
+                                icon={<HamburgerIcon />}
+                                size="lg"
+                                aria-label="Options"
+                            />
+                            <MenuList>
+                                <MenuItem
+                                    as={MenuLink}
+                                    href="http://join.seadclub.onlie"
+                                >
+                                    Bступити в клуб
+                                </MenuItem>
+                                <MenuItem as={MenuLink} href="https://t.me/seadclub_bot">
+                                    Зв'язатися з нами
+                                </MenuItem>
+                                <MenuItem
+                                    as={MenuLink}
+                                    href="https://github.com/seadclub"
+                                >
+                                    Github
+                                </MenuItem>
+                                <MenuItem
+                                    as={Link}
+                                    href="https://github.com/seadclub/seadclub.online"
+                                >
+                                    Source code
+                                </MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Box>
+                </Box>
+            </Container>
         </Box>
-      </Container>
-    </Box>
-  );
+    );
 };
 
 export default Navbar;
