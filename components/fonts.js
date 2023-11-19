057c702db3f364a6887423a9f488a9d4b258f2d4
kenjitheman <kenjith3man@gmail.com>
2023-11-19 22:16:39 +0200

    feat: Update font

--- components/fonts.js
+++ components/fonts.js
@@ -1,7 +1,7 @@
 const Fonts = () => (
   <style jsx global>
     {`
-    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500;600;700&display=swap');
   `}
   </style>
 );
