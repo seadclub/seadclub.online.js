6666fa003b2ad3f5159ff14f7286b921ac84d0db
kenjitheman <kenjith3man@gmail.com>
2023-11-19 22:22:17 +0200

    chore: Update article layout meta

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -23,7 +23,7 @@ const Layout = ({ children, title }) => {
         {title && (
           <Head>
             <title>{t}</title>
-            <meta name="twitter:title" content={t} />
+            <meta name="og:title" content={t} />
             <meta property="og:title" content={t} />
           </Head>
         )}
