e0398fb404f0a3fc3e413b1670ccd9af81b8d308
kenjitheman <kenjith3man@gmail.com>
2023-12-07 23:00:50 +0200

    feat: add speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -3,31 +3,33 @@ import Fonts from "../components/fonts";
 import { AnimatePresence } from "framer-motion";
 import Chakra from "../components/chakra";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 if (typeof window !== "undefined") {
-  window.history.scrollRestoration = "manual";
+    window.history.scrollRestoration = "manual";
 }
 
 function Website({ Component, pageProps, router }) {
-  return (
-    <Chakra cookies={pageProps.cookies}>
-      <Fonts />
-      <Layout router={router}>
-        <AnimatePresence
-          exitBeforeEnter
-          initial={true}
-          onExitComplete={() => {
-            if (typeof window !== "undefined") {
-              window.scrollTo({ top: 0 });
-            }
-          }}
-        >
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </Layout>
-      <Analytics />
-    </Chakra>
-  );
+    return (
+        <Chakra cookies={pageProps.cookies}>
+            <Fonts />
+            <Layout router={router}>
+                <AnimatePresence
+                    exitBeforeEnter
+                    initial={true}
+                    onExitComplete={() => {
+                        if (typeof window !== "undefined") {
+                            window.scrollTo({ top: 0 });
+                        }
+                    }}
+                >
+                    <Component {...pageProps} key={router.route} />
+                </AnimatePresence>
+            </Layout>
+            <Analytics />
+            <SpeedInsights />
+        </Chakra>
+    );
 }
 
 export default Website;
